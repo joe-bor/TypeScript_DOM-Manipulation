62df6f46eb6b125402adc537681ee6fd48ef0865
joe-bor <kimborlongan_08@yahoo.com>
2023-08-05 08:04:51 -0700

    TS project, DOM Manipulation

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import './css/style.css'
+import FullList from './model/FullList'
+import ListItem from './model/ListItem'
+import ListTemplate from './templates/ListTemplate'
+
+const initApp = (): void => {
+  const fullList = FullList.instance
+  const template = ListTemplate.instance
+
+  const itemEntryForm = document.getElementById('itemEntryForm') as HTMLFormElement
+
+  itemEntryForm.addEventListener('submit', (event: SubmitEvent): void => {
+    event.preventDefault()
+    
+  })
+
+  const clearItems = document.getElementById('clearItemsButton') as HTMLButtonElement
+
+  clearItems.addEventListener('click', (): void => {
+    fullList.clearList()
+    template.clear()
+  })
+
+  fullList.load()
+  template.render(fullList)
+}
+
+document.addEventListener('DOMContentLoaded', initApp) // defer
